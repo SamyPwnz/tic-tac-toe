7fb2fcfd3410eb07b471d9c3f3d14ef75d18a8ff
SamyPwnz <samuelsolis626@gmail.com>
2016-11-17 10:04:40 -0800

    I PUT DUH SHAPES BOI
    DUH SHAPES

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,11 +2,23 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 function makeShape() {
+
 var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cy" ,51)
-circle.setAttribute("cx" ,44)
+circle.setAttribute("cy" ,70)
+circle.setAttribute("cx" ,65)
 circle.setAttribute("r" ,35)
 circle.setAttribute("fill","red")
-var.canvas = document.getElementById("game-board")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
+
+function changeShape() {
+
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,70)
+circle.setAttribute("cx" ,255)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","red")
+var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
 }
