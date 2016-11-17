56a5e9e9cd499c363aaea8477a7dd0e8260fb136
SamyPwnz <samuelsolis626@gmail.com>
2016-11-17 10:18:40 -0800

    GO TO FINISH SHAPES
    GOT THREE SHAPES ROGHT NOW

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -16,9 +16,31 @@ function changeShape() {
 
 var circle = document.createElementNS(namespace, "circle")
 circle.setAttribute("cy" ,70)
+circle.setAttribute("cx" ,155)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","green")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
+
+function SumShape() {
+
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,70)
 circle.setAttribute("cx" ,255)
 circle.setAttribute("r" ,35)
 circle.setAttribute("fill","red")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
+
+function anotherShape() {
+
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,70)
+circle.setAttribute("cx" ,255)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","magenta")
 var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
 }
