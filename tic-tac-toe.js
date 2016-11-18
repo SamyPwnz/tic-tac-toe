fbff2c27c87255f5e9a1cd4259f250733c4de4ca
SamyPwnz <samuelsolis626@gmail.com>
2016-11-17 16:02:24 -0800

    I DID SUM STUUF

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,10 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-function Shape1() {
+var turn = "player1"
 
+function Shape1() {
+if (turn=="player1"){
 var circle = document.createElementNS(namespace, "circle")
 circle.setAttribute("cy" ,70)
 circle.setAttribute("cx" ,65)
@@ -10,10 +12,19 @@ circle.setAttribute("r" ,35)
 circle.setAttribute("fill","red")
 var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
+turn ="player2"}else{var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,70)
+circle.setAttribute("cx" ,155)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","green")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+turn ="player1"
+}
 }
 
 function Shape2() {
-
+if (turn=="player1"){
 var circle = document.createElementNS(namespace, "circle")
 circle.setAttribute("cy" ,70)
 circle.setAttribute("cx" ,155)
@@ -40,7 +51,7 @@ var circle = document.createElementNS(namespace, "circle")
 circle.setAttribute("cy" ,165)
 circle.setAttribute("cx" ,65)
 circle.setAttribute("r" ,35)
-circle.setAttribute("fill","red")
+circle.setAttribute("fill","green")
 var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
 }
@@ -63,6 +74,17 @@ circle.setAttribute("cy" ,165)
 circle.setAttribute("cx" ,255)
 circle.setAttribute("r" ,35)
 circle.setAttribute("fill","red")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
+
+function Shape7() {
+
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,165)
+circle.setAttribute("cx" ,55)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","green")
 var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
 }
