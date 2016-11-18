8a1e4edcc9e91b663d16d3734a1608c9377376f9
SamyPwnz <samuelsolis626@gmail.com>
2016-11-17 16:36:05 -0800

    DUH SHIZZLE
    FHCO;USDNC;O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,7 +14,7 @@ var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
 turn ="player2"}else{var circle = document.createElementNS(namespace, "circle")
 circle.setAttribute("cy" ,70)
-circle.setAttribute("cx" ,155)
+circle.setAttribute("cx" ,65)
 circle.setAttribute("r" ,35)
 circle.setAttribute("fill","green")
 var canvas = document.getElementById("game-board")
@@ -25,16 +25,28 @@ turn ="player1"
 
 function Shape2() {
 if (turn=="player1"){
+  alert("player 1")
 var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,70)
+circle.setAttribute("cx" ,155)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","red")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+turn ="player2"}else{
+  var circle = document.createElementNS(namespace, "circle")
 circle.setAttribute("cy" ,70)
 circle.setAttribute("cx" ,155)
 circle.setAttribute("r" ,35)
 circle.setAttribute("fill","green")
 var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
+turn ="player1"
+}
 }
 
 function Shape3() {
+if (turn=="player1"){
 
 var circle = document.createElementNS(namespace, "circle")
 circle.setAttribute("cy" ,70)
@@ -43,6 +55,16 @@ circle.setAttribute("r" ,35)
 circle.setAttribute("fill","red")
 var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
+turn ="player2"}else{
+  var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,70)
+circle.setAttribute("cx" ,155)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","green")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+turn ="player1"
+}
 }
 
 function Shape4() {
@@ -81,8 +103,30 @@ canvas.appendChild(circle)
 function Shape7() {
 
 var circle = document.createElementNS(namespace, "circle")
-circle.setAttribute("cy" ,165)
-circle.setAttribute("cx" ,55)
+circle.setAttribute("cy" ,255)
+circle.setAttribute("cx" ,65)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","green")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
+
+function Shape8() {
+
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,255)
+circle.setAttribute("cx" ,165)
+circle.setAttribute("r" ,35)
+circle.setAttribute("fill","red")
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
+
+function Shape9() {
+
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy" ,255)
+circle.setAttribute("cx" ,255)
 circle.setAttribute("r" ,35)
 circle.setAttribute("fill","green")
 var canvas = document.getElementById("game-board")
